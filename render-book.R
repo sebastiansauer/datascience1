391eceb1c7127c2b0321e4dd8027118d4ec94b05
Sebastian Sauer <ssauer@posteo.de>
2022-02-21 23:03:09 +0100

    update title page

--- render-book.R
+++ render-book.R
@@ -2,3 +2,6 @@ bookdown::preview_chapter("index.Rmd")
 
 bookdown::preview_chapter("01-Modulueberblick.Rmd")
 bookdown::preview_chapter("02-Modulzeitplan.Rmd")
+
+bookdown::render_book("index.Rmd", output_format = "bookdown::gitbook")
+bookdown::render_book("index.Rmd", output_format = "bookdown::pdf_book")
