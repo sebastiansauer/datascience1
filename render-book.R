4449f4d193adf7a925cc5e61110c94bfbdb0ad62
Sebastian Sauer <ssauer@posteo.de>
2022-03-10 22:48:16 +0100

    update

--- render-book.R
+++ render-book.R
@@ -1,5 +1,4 @@
-bookdown::preview_chapter("index.Rmd")
-
+bookdown::preview_chapter("03-Thema01.Rmd")
 bookdown::preview_chapter("01-Modulueberblick.Rmd")
 bookdown::preview_chapter("02-Modulzeitplan.Rmd")
 
