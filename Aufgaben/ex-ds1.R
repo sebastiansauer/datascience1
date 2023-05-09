d07365a76f2b6a82b2914a0d04945aee0798f24c
Sebastian Sauer <ssauer@posteo.de>
2023-05-09 18:27:20 +0200

    update

--- Aufgaben/ex-ds1.R
+++ Aufgaben/ex-ds1.R
@@ -44,6 +44,10 @@ teachertools::render_exs(ex_stat_learning[4],
                          render_yamlrmd = TRUE,
                          thema_nr = "1")
 
+
+
+
+
 # baueme ------------------------------------------------------------------
 
 
@@ -65,7 +69,7 @@ ex_baueme2 <-
     "bike04.Rmd")
 
 # Datenwerk:
-teachertools::render_exs( "tidymodels-penguins07.Rmd",
+teachertools::render_exs( "nasa06.Rmd",
                          my_edir = ex_dir,
                          output_path = path_datenwerk,
                          render_html = FALSE,
@@ -74,4 +78,78 @@ teachertools::render_exs( "tidymodels-penguins07.Rmd",
                          render_markdown = FALSE,
                          render_yamlrmd = TRUE,
                          thema_nr = "1")
+
+
+
+
+
+# Ensembles ---------------------------------------------------------------
+
+
+exs_ensemble <-
+  c(
+    "tidymodels-vorlage.Rmd",
+    "tidymodels-vorlage2.Rmd",
+    "tidymodels-tree3.Rmd",
+    "tidymodels-tree2.Rmd",   
+    "tidymodels-tree1.Rmd",   
+    "rf-usemodels.Rmd",
+    "rf-finalize2.Rmd",
+    "rf-finalize.Rmd",
+    "tidymodels3.Rmd",
+    "Bootstrap1.Rmd",
+    "diamonds-tidymodels01.Rmd"
+    )
+
+
+exs_ensemble2 <-
+  c(
+    "rf-finalize3.Rmd",
+    "wfsets_penguins01.Rmd",
+    "wfsets_penguins02.Rmd"
+  )
+
+
+# Datenwerk:
+teachertools::render_exs( exs_ensemble2,
+                          my_edir = ex_dir,
+                          output_path = path_datenwerk,
+                          render_html = FALSE,
+                          render_moodle = FALSE,
+                          render_pdf_print = FALSE,
+                          render_markdown = FALSE,
+                          render_yamlrmd = TRUE,
+                          thema_nr = "1")
+
+
+
+
+# Kaggle tmdb -------------------------------------------------------------
+
+
+exs_tmdb <-
+  c(
+    "tmdb01.Rmd",
+    "tmdb02.Rmd",    
+    "tmdb03.Rmd",    
+    "tmdb04.Rmd",    
+    "tmdb05.Rmd",
+    "tmdb06.Rmd",
+    "tmdb07.Rmd",
+    "tmdb08.Rmd"
+  )
+
+
+
+# Datenwerk:
+teachertools::render_exs(
+  exs_tmdb,
+  my_edir = ex_dir,
+  output_path = path_datenwerk,
+  render_html = FALSE,
+  render_moodle = FALSE,
+  render_pdf_print = FALSE,
+  render_markdown = FALSE,
+  render_yamlrmd = TRUE,
+  thema_nr = "1")
 
